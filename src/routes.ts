73eaf47c539038212cfe540274e26cc873c947e5
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-05-07 17:45:21 -0300

    feat: dependecy inversion principle

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import nodemailer from 'nodemailer'
-import { prisma } from './prisma';
+import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository';
+import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 
 export const routes = express.Router()
 
@@ -8,27 +9,36 @@ const transport = nodemailer.createTransport({
   host: "smtp.mailtrap.io",
   port: 2525,
   auth: {
-    user: "a42d7809a894ef",
-    pass: "9163864b34a845"
+    user: process.env.USER_NODEMAILER,
+    pass: process.env.PASS_NODEMAILER
   }
 });
 
 routes.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body
 
-  const feedback = 
+  const prismaFeedbackRepository = new PrismaFeedbacksRepository()
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+    prismaFeedbackRepository
+  )
 
-  await transport.sendMail({
-    from: 'Equipe Feedget <liliancmartinsfritzen@gmail.com',
-    to: 'Lílian Martins Fritzen liliancmartinsfritzen@gmail.com',
-    subject: 'Novo feedback',
-    html: [
-      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
-      `<p>Tipo do feedback: ${type}</p>`,
-      `<p>Comentário: ${comment}</p>`,
-      `</div>`
-    ].join('\n')
+  await submitFeedbackUseCase.execute({
+    type,
+    comment,
+    screenshot
   })
 
-  return res.status(201).json({ data: feedback })
+  // await transport.sendMail({
+  //   from: 'Equipe Feedget <liliancmartinsfritzen@gmail.com',
+  //   to: 'Lílian Martins Fritzen liliancmartinsfritzen@gmail.com',
+  //   subject: 'Novo feedback',
+  //   html: [
+  //     `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+  //     `<p>Tipo do feedback: ${type}</p>`,
+  //     `<p>Comentário: ${comment}</p>`,
+  //     `</div>`
+  //   ].join('\n')
+  // })
+
+  return res.status(201).send()
 })
