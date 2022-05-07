410148d75d2491070d921f94d1439def6e06be0d
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-05-07 14:53:45 -0300

    feat: configured email sending

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express'
+import nodemailer from 'nodemailer'
 import { prisma } from './prisma'
 
 const app = express()
 
 app.use(express.json())
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "a42d7809a894ef",
+    pass: "9163864b34a845"
+  }
+});
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body
@@ -14,6 +24,18 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot
     }
+  })
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <liliancmartinsfritzen@gmail.com',
+    to: 'Lílian Martins Fritzen liliancmartinsfritzen@gmail.com',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('\n')
   })
 
   return res.status(201).json({ data: feedback })
