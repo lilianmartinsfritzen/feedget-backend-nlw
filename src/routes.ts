81346fae7c3f047c0d16f0f5f1da7eb73a52ef7e
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-05-07 18:25:07 -0300

    refactor: send email with SOLID principles

--- src/routes.ts
+++ src/routes.ts
@@ -1,25 +1,20 @@
 import express from 'express'
 import nodemailer from 'nodemailer'
+import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter';
 import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository';
 import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 
 export const routes = express.Router()
 
-const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: process.env.USER_NODEMAILER,
-    pass: process.env.PASS_NODEMAILER
-  }
-});
-
 routes.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body
 
   const prismaFeedbackRepository = new PrismaFeedbacksRepository()
+  const nodemailerMailAdapter = new NodemailerMailAdapter()
+
   const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-    prismaFeedbackRepository
+    prismaFeedbackRepository,
+    nodemailerMailAdapter
   )
 
   await submitFeedbackUseCase.execute({
@@ -27,18 +22,6 @@ routes.post('/feedbacks', async (req, res) => {
     comment,
     screenshot
   })
-
-  // await transport.sendMail({
-  //   from: 'Equipe Feedget <liliancmartinsfritzen@gmail.com',
-  //   to: 'Lílian Martins Fritzen liliancmartinsfritzen@gmail.com',
-  //   subject: 'Novo feedback',
-  //   html: [
-  //     `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
-  //     `<p>Tipo do feedback: ${type}</p>`,
-  //     `<p>Comentário: ${comment}</p>`,
-  //     `</div>`
-  //   ].join('\n')
-  // })
 
   return res.status(201).send()
 })
